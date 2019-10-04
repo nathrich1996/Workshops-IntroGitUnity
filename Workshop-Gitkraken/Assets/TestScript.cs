7bda57c7d01f0faefe4429fd4d286449eca0dfe3
nathrichCSUF <31288234+nathrichCSUF@users.noreply.github.com>
2019-10-03 20:59:48 -0700

    Filled in Start()

--- Workshop-Gitkraken/Assets/TestScript.cs
+++ Workshop-Gitkraken/Assets/TestScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = 1;
     }
 
     // Update is called once per frame
