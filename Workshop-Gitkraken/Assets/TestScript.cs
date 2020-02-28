f8d5648bbb60597cc91489ee57645cd8ca204ef7
Nathaniel Richards <48138788+nathrich1996@users.noreply.github.com>
2020-02-27 22:57:54 -0800

    Changed Starting X
    X is now 1

--- Workshop-Gitkraken/Assets/TestScript.cs
+++ Workshop-Gitkraken/Assets/TestScript.cs
@@ -8,7 +8,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        x = 0;
+        x = 1;
     }
 
     // Update is called once per frame
