98060b013c455ba8c50b54ab614e365ef69d6010
Nathaniel Richards <48138788+nathrich1996@users.noreply.github.com>
2020-02-27 23:05:45 -0800

    Update TestScript.cs

--- Workshop-Gitkraken/Assets/TestScript.cs
+++ Workshop-Gitkraken/Assets/TestScript.cs
@@ -8,7 +8,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        x = 1;
+        x = 0;
     }
 
     // Update is called once per frame
